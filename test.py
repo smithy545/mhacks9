08edf875503c4cd41602cffebd4c4ad9f8b4a4ff
smithy545 <smithy545@gmail.com>
2017-03-25 01:04:25 -0400

    add test creation

--- test.py
+++ test.py
@@ -2,15 +2,19 @@ import requests
 import json
 
 # I'm helping
-customerId = 'your customerId here'
 apiKey = '6ef5c6d1654869455435f792cf9c0712'
 
-url = 'http://api.reimaginebanking.com/customers/{}/accounts?key={}'.format(customerId,apiKey)
+url = 'http://api.reimaginebanking.com/customers?key={}'.format(apiKey)
 payload = {
-  "type": "Savings",
-  "nickname": "test",
-  "rewards": 10000,
-  "balance": 10000,	
+    'first_name': 'Peter',
+    'last_name': 'Fedrizzi',
+    'address': {
+        'street_number': '619',
+        'street_name': 'West Peter Street',
+        'city': 'Peterville',
+        'state': 'OH',
+        'zip': '44106'
+    }
 }
 # Create a Savings Account
 response = requests.post( 
@@ -20,4 +24,4 @@ response = requests.post(
 	)
 
 if response.status_code == 201:
-    print('account created')
+    print('customer created')
